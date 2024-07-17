dccae23583c5ed0cf1c8be6add198214ca98213d
SlejmUr <slejmur@protonmail.com>
2024-07-17 14:35:12 +0200

    Helper for MP

---
+++ Convert/MPConvertHelper.cs
@@ -0,0 +1,20 @@
+﻿using EIV_JsonLib.Interfaces;
+using EIV_JsonMP;
+using MessagePack;
+
+namespace EIV_JsonLib.Convert;
+
+public static class MPConvertHelper
+{
+    public static byte[] Serialize<T>(T item) where T : IItem
+    {
+        var options = MessagePackSerializerOptions.Standard.WithResolver(JsonMPResolver.Instance);
+        return MessagePackSerializer.Serialize(item, options);
+    }
+
+    public static T Deserialize<T>(byte[] bytes) where T : IItem
+    {
+        var options = MessagePackSerializerOptions.Standard.WithResolver(JsonMPResolver.Instance);
+        return MessagePackSerializer.Deserialize<T>(bytes, options);
+    }
+}
